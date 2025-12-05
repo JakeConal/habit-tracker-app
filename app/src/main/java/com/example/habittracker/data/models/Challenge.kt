92fad5faf202e1c401322e790a9b22411b2f874a
JakeConal <23120124@student.hcmus.edu.vn>
2025-12-05 16:15:01 +0700

    feat(community): add challenge card UI and data models

---
+++ app/src/main/java/com/example/habittracker/data/models/Challenge.kt
@@ -0,0 +1,21 @@
+package com.example.habittracker.data.models
+
+data class Challenge(
+    val id: String,
+    val title: String,
+    val description: String,
+    val imgURL: String,
+    val duration: ChallengeDuration,
+    val participants: Int = 0,
+    val isJoined: Boolean = false,
+)
+
+enum class ChallengeDuration(val duration: String, val color: BadgeColor) {
+    SEVEN_DAYS("7 Days Challenge", BadgeColor.CYAN),
+    THIRTY_DAYS("30 Days Challenge", BadgeColor.GREEN),
+}
+
+enum class BadgeColor {
+    CYAN,
+    GREEN,
+}
